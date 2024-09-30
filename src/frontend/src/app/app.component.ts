3361a845370a6a42ea4de6753e5fd2fc068e2744
Daniel Vetter <daniel.vetter86@gmail.com>
2024-09-30 02:12:26 +0200

    Made ui more mobile friendly

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { MenuComponent } from './menu/menu.component';
 import { GlobalErrorHandlerDialogComponent } from "./global-error-handler-dialog/global-error-handler-dialog.component";
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
+import { TopBarComponent } from "./top-bar/top-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, MenuComponent, GlobalErrorHandlerDialogComponent, ToastModule],
+  imports: [RouterOutlet, ButtonModule, MenuComponent, GlobalErrorHandlerDialogComponent, ToastModule, TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/frontend/src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { MenuComponent } from '../menu/menu.component';
+import { NavigationSkipped, NavigationStart, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-top-bar',
+  standalone: true,
+  imports: [ButtonModule],
+  providers: [DialogService],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.scss'
+})
+export class TopBarComponent implements OnInit {
+
+  constructor(private dialogService: DialogService, private router: Router) { }
+
+  _dlg: DynamicDialogRef | undefined;
+
+  ngOnInit(): void {
+    this.router.events.subscribe(x => {
+      if (x instanceof NavigationSkipped ||
+        x instanceof NavigationStart
+      ) {
+        this.closeDialog();
+      }
+    });
+  }
+
+  onOpenMenuClicked() {
+    this._dlg = this.dialogService.open(MenuComponent, {
+      dismissableMask: true,
+      position: "left",
+      modal: true,
+      styleClass: "sideDialog",
+      transitionOptions: "0s",
+      showHeader: false
+    });
+  }
+
+  closeDialog() {
+    if (this._dlg !== undefined) {
+      this._dlg.close();
+      this._dlg = undefined;
+    }
+  }
+
+}
