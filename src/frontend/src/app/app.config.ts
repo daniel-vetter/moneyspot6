854473b69aa927cede6fb6d3bb9e67c00cc998cf
Daniel Vetter <daniel.vetter86@gmail.com>
2024-10-03 23:14:07 +0200

    Implemented PWA

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, ErrorHandler, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +6,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { provideHttpClient } from '@angular/common/http';
 import { GlobalErrorHandler } from './global-error-handler-dialog/global-error-handler';
+import { provideServiceWorker } from '@angular/service-worker';
 
 const initializeAppFactory = (primeConfig: PrimeNGConfig) => () => {
   primeConfig.ripple = true;
@@ -29,7 +30,10 @@ export const appConfig: ApplicationConfig = {
     },
     {
       provide: MessageService
-    }
+    }, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 };
 
