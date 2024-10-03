7268bf60a974f0cb3486d04f9eaf8afdf221b609
Daniel Vetter <daniel.vetter86@gmail.com>
2024-10-03 03:04:07 +0200

    Fixed some UI issues

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -24,6 +24,9 @@ export class AppComponent implements OnInit {
 
     h.setOptions({
       colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+      accessibility: {
+        enabled: false
+      },
       chart: {
         backgroundColor: "#18181b",
       },
