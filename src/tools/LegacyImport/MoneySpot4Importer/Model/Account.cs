8c5cdd7bd3041db7fc21b95a0926821d01fafc64
Daniel Vetter <daniel.vetter86@gmail.com>
2025-01-02 02:30:06 +0100

    Added legacy data importer

---
+++ src/tools/LegacyImport/MoneySpot4Importer/Model/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneySpot4Importer.Model
+{
+    public class Account
+    {
+        public object ImporterOptions { get; set; }
+        public string ImporterKey { get; set; }
+        public decimal Balance { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/tools/LegacyImport/MoneySpot4Importer/Model/Rule.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MoneySpot4Importer.Model
+{
+    public class Rule
+    {
+        public Rule()
+        {
+            Purpose = new StringFilter();
+            ContraAccount = new StringFilter();
+        }
+
+        public Account Account { get; set; }
+        public StringFilter Purpose { get; set; }
+        public StringFilter ContraAccount { get; set; }
+        public string Name { get; set; }
+        public string Script { get; set; }
+    }
+
+    public class StringFilter
+    {
+        public StringFilter()
+        {
+            Value = String.Empty;
+            IsRegEx = false;
+        }
+
+        public StringFilter(string val, bool isRegEx)
+        {
+            Value = val;
+            IsRegEx = isRegEx;
+        }
+
+        public string Value { get; set; }
+        public bool IsRegEx { get; set; }
+    }
+}
