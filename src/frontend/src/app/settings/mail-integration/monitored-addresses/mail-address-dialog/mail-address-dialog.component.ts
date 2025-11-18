3f8406aa02ceb5d366164bf2e776297bb733d5fc
Daniel Vetter <daniel.vetter86@gmail.com>
2025-11-18 23:13:11 +0100

    Fixed build error

--- src/frontend/src/app/settings/mail-integration/monitored-addresses/mail-address-dialog/mail-address-dialog.component.ts
+++ src/frontend/src/app/settings/mail-integration/monitored-addresses/mail-address-dialog/mail-address-dialog.component.ts
@@ -1,17 +1,12 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {ButtonModule} from "primeng/button";
-import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {InputText} from "primeng/inputtext";
-import {TextareaModule} from 'primeng/textarea';
-import {MessageModule} from "primeng/message";
-import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
-import {
-    CreateCategoryValidationErrorResponse,
-    CreateMonitoredAddressRequest, CreateMonitoredAddressValidationErrorResponse,
-    MailIntegrationClient, MonitoredAddressResponse,
-    UpdateCategoryValidationErrorResponse, UpdateMonitoredAddressRequest, UpdateMonitoredAddressValidationErrorResponse
-} from "../../../server";
-import {lastValueFrom} from "rxjs";
+import { Component, inject, OnInit } from '@angular/core';
+import { ButtonModule } from "primeng/button";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { InputText } from "primeng/inputtext";
+import { TextareaModule } from 'primeng/textarea';
+import { MessageModule } from "primeng/message";
+import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
+import { CreateMonitoredAddressRequest, CreateMonitoredAddressValidationErrorResponse, MailIntegrationClient, MonitoredAddressResponse, UpdateMonitoredAddressRequest, UpdateMonitoredAddressValidationErrorResponse } from "../../../../server";
+import { lastValueFrom } from "rxjs";
 
 @Component({
     selector: 'app-mail-address-dialog',
@@ -26,8 +21,8 @@ export class MailAddressDialogComponent implements OnInit {
 
     monitoredAddress: undefined | MonitoredAddressResponse;
     form = new FormGroup({
-        address: new FormControl<string>("", {nonNullable: true, validators: [Validators.required]}),
-        prompt: new FormControl<string>("", {nonNullable: true, validators: [Validators.required]})
+        address: new FormControl<string>("", { nonNullable: true, validators: [Validators.required] }),
+        prompt: new FormControl<string>("", { nonNullable: true, validators: [Validators.required] })
     });
 
     constructor() {
@@ -67,14 +62,13 @@ export class MailAddressDialogComponent implements OnInit {
                 })));
             }
             this.dialogRef.close(true);
-        } catch
-            (error) {
+        } catch (error) {
             if (error instanceof CreateMonitoredAddressValidationErrorResponse || error instanceof UpdateMonitoredAddressValidationErrorResponse) {
                 queueMicrotask(() => {
                     if (error.missingAddress)
-                        this.form.controls.address.setErrors({missingAddress: true});
+                        this.form.controls.address.setErrors({ missingAddress: true });
                     if (error.alreadyConfigured)
-                        this.form.controls.address.setErrors({alreadyConfigured: true});
+                        this.form.controls.address.setErrors({ alreadyConfigured: true });
                 });
             }
         }
