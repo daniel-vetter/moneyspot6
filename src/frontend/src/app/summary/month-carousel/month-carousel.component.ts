a2f59ec83648fef59cc60cb73c455932833f5881
Daniel Vetter <daniel.vetter86@gmail.com>
2026-01-04 03:12:48 +0100

    Dashboard mobile ui improvments

--- src/frontend/src/app/summary/month-carousel/month-carousel.component.ts
+++ src/frontend/src/app/summary/month-carousel/month-carousel.component.ts
@@ -1,23 +1,28 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { MonthSummaryResponse, SummaryPageClient } from '../../server';
 import { PanelModule } from "primeng/panel";
-import { filter, lastValueFrom } from 'rxjs';
+import {filter, lastValueFrom, Subscription} from 'rxjs';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { ValueComponent } from '../../common/value/value.component';
 import { AppEvents } from '../../app-events';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {BreakpointObserver, LayoutModule} from "@angular/cdk/layout";
 
 @Component({
     selector: 'app-month-carousel',
-    imports: [CarouselModule, PanelModule, ValueComponent, ScrollPanelModule],
+    imports: [CarouselModule, PanelModule, ValueComponent, ScrollPanelModule, LayoutModule],
     templateUrl: './month-carousel.component.html',
     styleUrl: './month-carousel.component.scss'
 })
-export class MonthCarouselComponent implements OnInit {
+export class MonthCarouselComponent implements OnInit, OnDestroy {
 
     summaryPageClient = inject(SummaryPageClient);
     appEvents = inject(AppEvents);
+    breakpointObserver: BreakpointObserver = inject(BreakpointObserver);
+    tileCount = 1;
+    showNavigator = false;
+    mediaObserverSubscription: Subscription | undefined;
 
     constructor() {
         this.appEvents.events
@@ -25,10 +30,28 @@ export class MonthCarouselComponent implements OnInit {
             .subscribe(async () => {
                 await this.update();
             })
+
+
     }
 
     async ngOnInit(): Promise<void> {
+        this.mediaObserverSubscription = this.breakpointObserver.observe('(max-width: 1000px)').subscribe(x => {
+            if (x.matches) {
+                this.tileCount = 1;
+                this.showNavigator = false;
+            } else {
+                this.tileCount = 4;
+                this.showNavigator = true;
+            }
+        });
+
         await this.update();
+    }
+
+    ngOnDestroy(): void {
+        if (this.mediaObserverSubscription) {
+            this.mediaObserverSubscription.unsubscribe();
+        }
     }
 
     private async update() {
